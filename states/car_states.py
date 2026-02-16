c376fe3accf0107a4f95e9b336775dfb3aa59c1b
webfox13rus <webfox13rus@gmail.com>
2026-02-16 14:05:18 +0300

    Update car_states.py

--- states/car_states.py
+++ states/car_states.py
@@ -7,12 +7,6 @@ class AddCarStates(StatesGroup):
     waiting_for_name = State()
     waiting_for_mileage = State()
     waiting_for_fuel_type = State()
-    waiting_for_confirm = State()
-
-class EditCarStates(StatesGroup):
-    waiting_for_car_choice = State()
-    waiting_for_field_choice = State()
-    waiting_for_new_value = State()
 
 class MileageUpdateStates(StatesGroup):
     waiting_for_car_choice = State()
