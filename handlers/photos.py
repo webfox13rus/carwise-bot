55d6d4052e84e1b3f89dc879e3bd7fa5ebca1f18
webfox13rus <webfox13rus@gmail.com>
2026-03-11 15:07:48 +0300

    Update photos.py

--- handlers/photos.py
+++ handlers/photos.py
@@ -1,6 +1,5 @@
 import logging
 from aiogram import Router, types, F
-from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
